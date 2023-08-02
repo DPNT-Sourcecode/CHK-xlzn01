daff97f2f130d246b7efa7bae8683b76148a05b0
sbx_user1051 <sbx_user1051@169.254.128.189>
2023-08-02 14:40:41 +0000

    Wed Aug 02 14:40:41 UTC 2023

--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -10,5 +10,11 @@ class TestHello(unittest.TestCase):
     def test_returns_a_hello_string(self):
         self.assertIn("hello", hello_solution.hello("").lower())
 
-    def test_returns_hello_world_regardless(self):
-        self.assertEqual(hello_solution.hello(""), "Hello, World!")
+    def test_returns_hello_with_friend_name(self):
+        friend_name: str = "John"
+
+        self.assertEquals(f"Hello, {friend_name}!", hello_solution.hello(friend_name=friend_name))
+
+    def test_raises_error_when_null_friend_name(self):
+        with self.assertRaises(AssertionError):
+            hello_solution.hello(friend_name=None)
