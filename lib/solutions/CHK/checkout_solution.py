c7a276973ab70ec78847b153e8c355dec8814e7e
sbx_user1051 <sbx_user1051@169.254.128.189>
2023-08-02 14:58:19 +0000

    Wed Aug 02 14:58:19 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,16 +11,22 @@ special_by_sku: Dict[str, Tuple[int, int]] = {
     "A": Specials(quantity=3, price=130), "B": Specials(quantity=2, price=45)
 }
 
-def get_count_and_remove(input: str, letter: str) -> int:
-    count: int = input.count(letter)
-    result: str = input.replace(letter)
-    return result
+
+def get_count_and_remove(skus: str, letter: str) -> Tuple[str, int]:
+    count: int = skus.count(letter)
+    # Replace the input value
+    result = skus.replace(letter, "")
+    return result, count
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
     result: int = -1
-    num_a = skus.count("a")
+    skus, num_a = get_count_and_remove(skus=skus, letter="a")
+    skus, num_b = get_count_and_remove(skus=skus, letter="b")
+    skus, num_c = get_count_and_remove(skus=skus, letter="c")
+    skus, num_d = get_count_and_remove(skus=skus, letter="d")
 
     return result
+
--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -3,6 +3,28 @@ import unittest
 from lib.solutions.CHK import checkout_solution
 
 
+class TestGetCountAndRemove(unittest.TestCase):
+    def test_with_no_values_returns_zero_and_empty_string(self):
+        ins, count = checkout_solution.get_count_and_remove("", "a")
+        self.assertEqual(ins, "")
+        self.assertEqual(count, 0)
+
+    def test_with_no_matching_values_returns_zero_and_original_string(self):
+        ins, count = checkout_solution.get_count_and_remove("a", "b")
+        self.assertEqual(ins, "a")
+        self.assertEqual(count, 0)
+
+    def test_with_matching_values_returns_one_and_empty_string(self):
+        ins, count = checkout_solution.get_count_and_remove("a", "a")
+        self.assertEqual(ins, "")
+        self.assertEqual(count, 1)
+
+    def test_with_matching_values_returns_one_and_remaining_string(self):
+        ins, count = checkout_solution.get_count_and_remove("ab", "a")
+        self.assertEqual(ins, "b")
+        self.assertEqual(count, 1)
+
 class TestCheckoutSolution(unittest.TestCase):
     def test_initial(self):
         pass
+
