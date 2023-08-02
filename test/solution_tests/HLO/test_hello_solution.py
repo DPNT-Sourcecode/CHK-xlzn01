baa52c49674e2e3a64bccf3f6111d7d98757f5ad
sbx_user1051 <sbx_user1051@169.254.128.189>
2023-08-02 14:34:20 +0000

    Wed Aug 02 14:34:20 UTC 2023

--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -2,6 +2,10 @@ import unittest
 
 from lib.solutions.HLO import hello_solution
 
+
 class TestHello(unittest.TestCase):
     def test_returns_a_string(self):
         self.assertTrue(isinstance(hello_solution.hello(""), str))
+
+    def test_returns_a_hello_string(self):
+        self.assertIn("hello", hello_solution.hello("").lower())
