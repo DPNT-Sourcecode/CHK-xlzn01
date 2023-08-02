22c6c0ed1acc75d2de6dfbb1262f78dbcb43070d
sbx_user1051 <sbx_user1051@169.254.128.189>
2023-08-02 14:52:19 +0000

    Wed Aug 02 14:52:19 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,22 @@
+from typing import Dict, Tuple, NamedTuple
+from collections import namedtuple
+
+Specials = namedtuple(typename="Specials", field_names=("quantity", "price"))
+
+stock_prices_by_sku: Dict[str, int] = {
+    "A": 50, "B": 30, "C": 20, "D": 15
+}
+
+special_by_sku: Dict[str, Tuple[int, int]] = {
+    "A": Specials(quantity=3, price=130), "B": Specials(quantity=2, price=45)
+}
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    result: int = -1
+    num_a = skus.count("a")
+
+    return result
+
---
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -0,0 +1,8 @@
+import unittest
+
+from lib.solutions.CHK import checkout_solution
+
+
+class TestCheckoutSolution(unittest.TestCase):
+    def test_initial(self):
+        pass
