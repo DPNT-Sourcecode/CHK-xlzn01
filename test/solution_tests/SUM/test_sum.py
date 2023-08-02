ade8ef9dc45d296dd18cf0fc5c19366398708993
sbx_user1051 <sbx_user1051@169.254.128.189>
2023-08-02 14:28:36 +0000

    Wed Aug 02 14:28:36 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -10,3 +10,10 @@ class TestSum(unittest.TestCase):
     def test_sum_with_two_and_three_equals_five(self):
         self.assertEquals(sum_solution.compute(2, 3), 5)
 
+    def test_sum_with_zero_and_one_returns_one(self):
+        self.assertEquals(sum_solution.compute(0, 1), 1)
+
+    def test_sum_with_five_and_six_returns_eleven(self):
+        self.assertEquals(sum_solution.compute(5, 6), 11)
+
+
