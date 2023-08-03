3474ce71482cea81aedc774190756a8331ab85bb
sbx_user1051 <sbx_user1051@169.254.197.117>
2023-08-03 08:34:23 +0000

    Thu Aug 03 08:34:23 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,43 +2,34 @@ from typing import Dict, Tuple, NamedTuple
 from collections import namedtuple
 
 price_table = {
-    "A": {"price": 50, "specials": {"3A": 130, "5A": 200}},
-    "B": {"price": 30, "specials": {"2B": 45}},
-    "C": {"price": 20},
-    "D": {"price": 15},
-    "E": {"price": 40, "specials": {"2E": "B"}}
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+    "E": 40
+}
+
+multibuy_specials = {
+    "A": [(3, 130), (5, 200)],
+    "B": [(2, 45)]
+}
+
+freebees_specials = {
+    "E": [(2, "1B")]
 }
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
-    # Calculate total price without any special offers.
-    total_price: int = 0
-    for item in skus:
-        # Invalid product inside.
-        if item not in price_table:
-            return -1
-        total_price += price_table[item]["price"]
+    # Initialise the items into a dictionary with counts.
+    items: Dict[str, int] = {item: skus.count(item) for item in skus}
+    print(items)
 
-    for item in price_table:
-        # If there is a special for the item.
-        if "specials" in price_table[item]:
-            for offer in price_table[item]["specials"]:
-                # Determine then number of special items in the 'skus'.
-                count: int = skus.count(item)
+    return -1
 
-                # Compare against the number required for the special '3A' = 3.
-                if count >= int(offer[0]):
-                    # Compare against the item required.
-                    if offer[-1] in skus:
-                        count_free_item: int = skus.count(offer[-1])
-                        num_times: int = count // int(offer[0])
-                        num_times: int = min(num_times, count_free_item)
-                        total_price -= num_times * price_table[offer[-1]]["price"]
 
-                        # Remove used items from the basket.
-                        for i in range(num_times):
-                            skus = skus.replace(offer[-1], '', 1)
+if __name__ == '__main__':
+    basket: str = "AABB"
 
-    return total_price
+    print(checkout(skus=basket))
